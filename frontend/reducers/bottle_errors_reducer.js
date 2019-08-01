cfbe233d8a52a665966ed74641629e176cb4009e
Jessica White <jessica.jm.white@gmail.com>
2019-08-01 15:47:00 -0400

    add bottle actions and reducers

---
+++ frontend/reducers/bottle_errors_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_BOTTLE, RECEIVE_BOTTLE_ERRORS, CLEAR_BOTTLE_ERRORS } from '../actions/bottle_actions';
+import { merge } from 'lodash';
+
+const bottleErrorsReducer = (state, action) => {
+  prevState = Object.freeze(state);
+  let newState;
+  switch (action.type) {
+    case RECEIVE_BOTTLE_ERRORS:
+      newState = merge([], action.errors)
+      return newState;
+    case RECEIVE_BOTTLE:
+    case CLEAR_BOTTLE_ERRORS:
+      newState = [];
+      return newState;
+    default:
+      return state;
+  }
+}
+
+export default bottleErrorsReducer;
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
+import bottlesReducer from './bottles_reducer';
 
 export default combineReducers({
   users: usersReducer,
+  bottles: bottlesReducer,
 });
